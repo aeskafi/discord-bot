c931a92ddbf245561bb14fdf69c53fc9ff191ab8
Arham <arham.eskafi@gmail.com>
2022-07-10 16:14:11 +0000

    Feat reminder base on time was available now!

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import json
 import random
 from replit import db
 from keep_alive import keep_alive
+# for reminder function
+from discord.ext import commands
+import asyncio
+from datetime import datetime
 
 client = discord.Client()
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
@@ -91,6 +95,46 @@ async def on_message(message):
       db["responding"] = False
       await message.channel.send("Responding is off.")
 
+
+  if msg.startswith('remind'):
+    time = msg.split('remind ', 1)[1]
+    reminder = "Task"
+    print(time)
+    print(reminder)
+    user = message.author
+    embed = discord.Embed(color=0x55a7f7, timestamp=datetime.utcnow())
+    embed.set_footer(text="If you have any questions, suggestions or bug reports, please send me a message on Discord: @arham#2184", icon_url=f"{client.user.avatar_url}")
+    seconds = 0
+    if reminder is None:
+        embed.add_field(name='Warning', value='Please specify what do you want me to remind you about.') # Error message
+    if time.lower().endswith("d"):
+        seconds += int(time[:-1]) * 60 * 60 * 24
+        counter = f"{seconds // 60 // 60 // 24} days"
+    if time.lower().endswith("h"):
+        seconds += int(time[:-1]) * 60 * 60
+        counter = f"{seconds // 60 // 60} hours"
+    elif time.lower().endswith("m"):
+        seconds += int(time[:-1]) * 60
+        counter = f"{seconds // 60} minutes"
+    elif time.lower().endswith("s"):
+        seconds += int(time[:-1])
+        counter = f"{seconds} seconds"
+    if seconds == 0:
+        embed.add_field(name='Warning',
+                        value='Please specify a proper duration, send `reminder_help` for more information.')
+    elif seconds < 300:
+        embed.add_field(name='Warning',
+                        value='You have specified a too short duration!\nMinimum duration is 5 minutes.')
+    elif seconds > 7776000:
+        embed.add_field(name='Warning', value='You have specified a too long duration!\nMaximum duration is 90 days.')
+    else:
+      await message.channel.send(f"Alright, I will remind you about {reminder} in {counter}.")
+
+      await asyncio.sleep(seconds)
+      await message.channel.send(f"Hi, you asked me to remind you about {reminder} {counter} ago.")
+      return
+    # await ctx.send(embed=embed)
+    await message.channel.send(embed=embed)
 
 TOKEN = os.environ['TOKEN']
 keep_alive()
