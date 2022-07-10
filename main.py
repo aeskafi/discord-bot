b2d70067f0eb767a9cbf6100de1a46c7eb34d124
Arham <arham.eskafi@gmail.com>
2022-07-10 12:44:21 +0000

    Add inspiration quote responder ;)

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import discord
 import os
+import requests
+import json 
 
 client = discord.Client()
 
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return quote
+  
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -12,9 +20,12 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  # if message.content.startswith('$hello'):
+  #   await message.channel.send('Hello!')
 
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
 
 TOKEN = os.environ['TOKEN']
 client.run(TOKEN)
